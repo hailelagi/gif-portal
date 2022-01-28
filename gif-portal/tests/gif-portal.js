215ed0072cb6d0a015c351a7f5f43e322382a38c
hailelagi <52631736+hailelagi@users.noreply.github.com>
2022-01-28 11:35:37 +0100

    setup contract/test

--- gif-portal/tests/gif-portal.js
+++ gif-portal/tests/gif-portal.js
@@ -1,10 +1,8 @@
 const anchor = require('@project-serum/anchor');
 
+
+/*
 describe('gif-portal', () => {
-
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.GifPortal;
@@ -12,3 +10,28 @@ describe('gif-portal', () => {
     console.log("Your transaction signature", tx);
   });
 });
+*/
+
+async function main() {
+  console.log("starting...")
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.GifPortal;
+  const tx = await program.rpc.initialize();
+
+  console.log("tx signature =>", tx)
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
