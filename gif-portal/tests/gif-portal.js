94ac2dd2d7a4660618fcd3db53e47751ce0bcdeb
hailelagi <52631736+hailelagi@users.noreply.github.com>
2022-01-28 13:31:55 +0100

    feat: solana program state change

--- gif-portal/tests/gif-portal.js
+++ gif-portal/tests/gif-portal.js
@@ -1,5 +1,5 @@
 const anchor = require('@project-serum/anchor');
-
+const { SystemProgram } = anchor.web3;
 
 /*
 describe('gif-portal', () => {
@@ -12,16 +12,45 @@ describe('gif-portal', () => {
 });
 */
 
+/* 
+Anchor abstracts away:
+- JSON RPC wrapper call to a solana cluster(local)
+- interfacing with solana/web3 api
+- interfacing with solana
+ */
+
 async function main() {
   console.log("starting...")
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.GifPortal;
-  const tx = await program.rpc.initialize();
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount]
+  });
 
   console.log("tx signature =>", tx)
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("Gif count", account.totalGifs.toString())
+
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey
+    }
+  })
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey)
+  console.log("gif state change", account.totalGifs.toString())
 };
 
 const runMain = async () => {
